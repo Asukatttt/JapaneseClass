3f0bca7ca79742330e6400736d2a860b481b391a
asuka <asu.fitness.t@gmail.com>
2025-08-19 17:25:07 +0900

    fix:trialの箇所

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -37,18 +37,20 @@ function PaymentPage() {
       <h1 className="text-4xl font-bold mb-6">Payment Page</h1>
 
       {price && name ? (
-        <div className="bg-white shadow-md rounded-xl p-8 text-center max-w-md mb-6">
-          <p className="text-2xl font-semibold mb-4">{name}</p>
-          <p className="text-3xl font-bold mb-6">${price} / month</p>
+      <div className="bg-white shadow-md rounded-xl p-8 text-center max-w-md mb-6">
+        <p className="text-2xl font-semibold mb-4">{name}</p>
+        <p className="text-3xl font-bold mb-6">
+          ${price} {name?.includes('Trial') ? '/ one-time' : '/ month'}
+        </p>
 
-          <a
-            href="https://wise.com/pay/me/hiyoria14"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 transition mb-4"
-          >
-            Pay with Wise
-          </a>
+        <a
+          href="https://wise.com/pay/me/hiyoria14"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 transition mb-4"
+        >
+          Pay with Wise
+        </a>
 
           <p className="text-lg text-black-600 font-semibold mt-6">
             After completing your payment, please contact me at the email below.
@@ -78,13 +80,13 @@ function PaymentPage() {
         <p className="text-2xl mb-6">No plan selected</p>
       )}
 
-      <Link href="/">
+      <Link href="/priceList">
         <motion.button
           className="bg-[#1D3658] text-[#F2FAEF] font-semibold px-8 py-4 rounded-lg shadow-lg hover:scale-105 transition-transform"
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1, transition: { duration: 0.6, delay: 0.2 } }}
         >
-          Back to Home
+          Back to Page
         </motion.button>
       </Link>
     </div>
